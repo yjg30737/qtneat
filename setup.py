e8cdcffa58811f5e83008168cd837f105def7032
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-28 11:01:01 +0900

    QtPy -> QtAwesome, add toast/drawer

--- setup.py
+++ setup.py
@@ -19,6 +19,6 @@ setup(
     long_description_content_type='text/markdown',
     long_description=long_description,
     install_requires=[
-        'qtpy>=0.0.1'
+        'qtawesome>=0.0.1'
     ]
 )
