c71c283e58219f5ff92b77a8ca7eed6d11dde23d
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-12 18:16:16 +0900

    Refactoring, let user able to choose the folding option

--- qtneat/window/toolBar.py
+++ qtneat/window/toolBar.py
@@ -6,36 +6,51 @@ from pyqt_svg_button import SvgButton
 class ToolBar(QToolBar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__initVal()
         self.__initUi()
 
+    def __initVal(self):
+        self.__foldable_flag = False
+        self.__foldBtn = SvgButton()
+        self.__cornerAction = QWidgetAction(self)
+        self.__menuAnimation = QPropertyAnimation(self, b"height")
+
     def __initUi(self):
+        # moving toolbar is not standard nowadays, in my opinion
+        # at least I have to polish ugly looking style
         self.setMovable(False)
 
-        self.__foldBtn = SvgButton()
-        self.__foldBtn.setIcon('ico/fold.svg')
-        self.__foldBtn.setCheckable(True)
-        self.__foldBtn.toggled.connect(self.__fold)
-        self.__foldBtn.setMaximumWidth(12)
-
-        cornerWidget = QWidget()
-        lay = QHBoxLayout()
-        lay.addWidget(self.__foldBtn)
-        lay.setAlignment(Qt.AlignRight | Qt.AlignBottom)
-        lay.setContentsMargins(0, 0, 0, 0)
-        cornerWidget.setLayout(lay)
-        cornerWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-
-        self.__foldAction = QWidgetAction(self)
-        self.__foldAction.setDefaultWidget(cornerWidget)
-
-        self.addAction(self.__foldAction)
-
-        self.__menuAnimation = QPropertyAnimation(self, b"height")
-        self.__menuAnimation.valueChanged.connect(self.setFixedHeight)
-
+    def addWidget(self, widget: QWidget) -> QAction:
+        self.insertWidget(self.__cornerAction, widget)
         self.__menuAnimation.setStartValue(self.sizeHint().height())
-        self.__menuAnimation.setDuration(200)  # default duration
-        self.__menuAnimation.setEndValue(self.__foldBtn.sizeHint().height())  # default end value
+
+    def setFoldable(self, f: bool):
+        self.__foldable_flag = f
+        if self.__foldable_flag:
+            self.__foldBtn.setIcon('ico/fold.svg')
+            self.__foldBtn.setCheckable(True)
+            self.__foldBtn.toggled.connect(self.__fold)
+            self.__foldBtn.setMaximumWidth(12)
+
+            cornerWidget = QWidget()
+            lay = QHBoxLayout()
+            lay.addWidget(self.__foldBtn)
+            lay.setAlignment(Qt.AlignRight | Qt.AlignBottom)
+            lay.setContentsMargins(0, 0, 0, 0)
+            cornerWidget.setLayout(lay)
+            cornerWidget.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+
+            self.__cornerAction.setDefaultWidget(cornerWidget)
+
+            self.addAction(self.__cornerAction)
+
+            self.__menuAnimation.valueChanged.connect(self.setFixedHeight)
+            self.__menuAnimation.setStartValue(self.sizeHint().height())
+            self.__menuAnimation.setDuration(200)  # default duration
+            self.__menuAnimation.setEndValue(self.__foldBtn.sizeHint().height())  # default end value
+        else:
+            # remove fold button or add tack button
+            pass
 
     def __fold(self, f):
         if f:
@@ -49,6 +64,3 @@ class ToolBar(QToolBar):
             self.__foldBtn.setIcon('ico/fold.svg')
             self.setFixedHeight(self.sizeHint().height())
 
-    def addWidget(self, widget: QWidget) -> QAction:
-        self.insertWidget(self.__foldAction, widget)
-        self.__menuAnimation.setStartValue(self.sizeHint().height())
